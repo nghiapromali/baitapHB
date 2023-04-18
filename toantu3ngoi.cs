f06368ac3f38d78e283923f22103fbbd003e88aa
nghiapromali <122343003+nghiapromali@users.noreply.github.com>
2023-04-18 19:23:18 +0700

    Create toantu3ngoi.cs
    khai báo kiểu dữ liệu boolean , kiểm tra và in ra màn hình *không sử dụng if*

---
+++ toantu3ngoi.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text;
+
+namespace siuuuuuuuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //khai báo kiểu dữ liệu bool, kiểm tra và in ra màn hình *không sử dụng if
+            Console.OutputEncoding = Encoding.UTF8; // Mã hoá ký tự đầu ra thành UTF-8 
+            bool traffic_jam = true; //giá trị: 1
+            bool traffic_clear = false; //giád trị: 0
+
+            //Toán tử ba ngôi: <biểu thức điều kiện>?<giá trị trả về nếu true> : <giá trị trả về nếu false>
+            Console.WriteLine("Giao thông vào giờ cao điểm {0}", traffic_jam ? "tắc nghẽn" : "thông thoáng"); //In ra tắc nghẽn vì giá trị trả về của biểu thức là true
+            Console.WriteLine("Giao thông vào sáng sớm {0}", traffic_clear ? "tắc nghẽn" : "thông thoáng"); //In ra thông thoáng vì giá trị trà về của biểu thức là false
+        }
+    }
+}
