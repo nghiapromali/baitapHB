c7e946e3a6cccf91c9e618cafe64e853deff1a54
nghiapromali <122343003+nghiapromali@users.noreply.github.com>
2023-04-18 23:05:52 +0700

    Update toantu3ngoi.cs

--- toantu3ngoi.cs
+++ toantu3ngoi.cs
@@ -8,13 +8,13 @@ namespace siuuuuuuuu
         static void Main(string[] args)
         {
             //khai báo kiểu dữ liệu bool, kiểm tra và in ra màn hình *không sử dụng if
-            Console.OutputEncoding = Encoding.UTF8; // Mã hoá ký tự đầu ra thành UTF-8 
-            bool traffic_jam = true; //giá trị: 1
-            bool traffic_clear = false; //giád trị: 0
+            Console.OutputEncoding = Encoding.UTF8; // Mã hoá ký tự đầu ra UTF-8 
+            bool traffic_jam = true; //1
+            bool traffic_clear = false; //0
 
             //Toán tử ba ngôi: <biểu thức điều kiện>?<giá trị trả về nếu true> : <giá trị trả về nếu false>
-            Console.WriteLine("Giao thông vào giờ cao điểm {0}", traffic_jam ? "tắc nghẽn" : "thông thoáng"); //In ra tắc nghẽn vì giá trị trả về của biểu thức là true
-            Console.WriteLine("Giao thông vào sáng sớm {0}", traffic_clear ? "tắc nghẽn" : "thông thoáng"); //In ra thông thoáng vì giá trị trà về của biểu thức là false
+            Console.WriteLine("Giao thông vào giờ cao điểm {0}", traffic_jam ? "tắc nghẽn" : "thông thoáng"); //giá trị trả về của biểu thức là true -> In ra tắc nghẽn
+            Console.WriteLine("Giao thông vào sáng sớm {0}", traffic_clear ? "tắc nghẽn" : "thông thoáng"); //giá trị trà về của biểu thức là false -> In ra thông thoáng
         }
     }
 }
